ad34d806a4795fcf76683127479f9dab2a44188f
Mateo Mejia <mateomgj@gmail.com>
2016-03-07 18:21:57 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,37 @@
 class UsersController < ApplicationController
-  def new
+
+  include UsersHelper
+  
+  def index
   end
+
+  def show
+  	@user=User.find(params[:id])
+  	
+  end
+
+  def new
+  	@user=User.new
+  end
+
+  def edit
+  end
+
+  def create
+  	@user=User.new(user_param)
+  	if @user.save
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+ def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+
+ private 
+ def user_param
+	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+ end
+
 end
