0bfa715e8b85b6d38983c656de56cf69468d7d0f
Mateo Mejia <mateomgj@gmail.com>
2016-03-10 15:28:09 -0500

    Chapter 8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user=User.find_by(email: params[:session][:email].downcase)
-  	if user && user.authenticate(params[:session][:password])
-  		log_in user
-  		#params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  		remember(user) if params[:session][:remember_me]=='1'  #This works and forget is not redundant
-  		redirect_to user 
+  	@user=User.find_by(email: params[:session][:email].downcase)
+  	if @user && @user.authenticate(params[:session][:password])
+  		log_in @user
+  		#params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+  		remember(@user) if params[:session][:remember_me]=='1'  #This works and forget is not redundant
+  		redirect_to @user 
   	else
   	flash.now[:danger] = 'Invalid email/password combination'
   	 render 'new'
