7bb849259b29081470949aee45d445b858be13f8
Mateo Mejia <mateomgj@gmail.com>
2016-04-26 14:49:30 -0500

    Finish all tests

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -18,7 +18,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
-    assert_match @user.followers.count, response.body
-    assert_match @user.following.count, response.body
+    assert_match @user.followers.count.to_s, response.body
+    assert_match @user.following.count.to_s, response.body
   end
 end
